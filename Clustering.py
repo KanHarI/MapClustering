68cf3ed81a98c21a8feb43bb5f30d6e192c44551
DESKTOP-U7D79JB\KanHar <knaan.harpaz@gmail.com>
2018-08-25 18:38:14 +0300

    Added support for different distance calculation

--- Clustering.py
+++ Clustering.py
@@ -2,11 +2,31 @@
 import scipy.spatial
 import math
 import copy
+import numpy
 
-def clustering(points, num_neighbors):
+def L2_distance(p1, p2):
+	dx = p1[0]-p2[0]
+	dy = p1[1]-p2[1]
+	return math.sqrt(dx**2+dy**2)
+
+
+def taxicab_distance(p1, p2):
+	dx = p1[0]-p2[0]
+	dy = p1[1]-p2[1]
+	return abs(dx)+abs(dy)
+
+
+distance_normalizer = []
+
+
+def clustering(points, num_neighbors, distance_func = L2_distance):
 	assert len(points) > num_neighbors
 	print("Creating local maps...")
-	local_maps = {i:generate_local_map_from_point(i,points,num_neighbors) for i in range(len(points))}
+	local_maps = {i:generate_local_map_from_point(i,points,num_neighbors,distance_func) for i in range(len(points))}
+	
+	global distance_normalizer
+	distance_normalizer = numpy.mean(distance_normalizer)
+
 	print("Running search:")
 	index_translation = {i:i for i in range(len(points))}
 	uncovered_indices = list(range(len(points)))
@@ -39,8 +59,8 @@ def clustering(points, num_neighbors):
 				points_with_hull_in_local_map.append(i)
 
 
-		fit_pt = max(points_with_hull_in_local_map, key = lambda p: pt_fitness(p, uncovered_indices, local_maps, hull, hull_angles, com, points))
-		radius = max(map(lambda p: L2_distance(points[fit_pt], points[p]), local_maps[fit_pt]))
+		fit_pt = max(points_with_hull_in_local_map, key = lambda p: pt_fitness(p, uncovered_indices, local_maps, hull, hull_angles, com, points, distance_func))
+		radius = max(map(lambda p: distance_func(points[fit_pt], points[p]), local_maps[fit_pt]))
 		# radius is in distance of lat&long...
 		fit_pts.append({"pt": fit_pt, "neighbors": local_maps[fit_pt], "radius": radius})
 
@@ -61,23 +81,21 @@ def find_center_of_mass(pts):
 # Trying to minimize "Surface tension"
 # Should probably add more paramters and improve this function
 # maybe try to find distance of local mapped points from center of mass?
-def pt_fitness(pt, uncovered_indices, local_maps, hull, hull_angles, com, points):
+def pt_fitness(pt, uncovered_indices, local_maps, hull, hull_angles, com, points, distance_func):
 	fitness = 0
 	for i in local_maps[pt]:
 		if i in uncovered_indices:
-			fitness += L2_distance(points[i], com)*0.1
+			fitness += distance_func(points[i], com)/distance_normalizer*2.04
 		if i in hull:
 			fitness += 20*hull_angles[i]
 	return fitness
 
-
-def L2_distance(p1, p2):
-	dx = p1[0]-p2[0]
-	dy = p1[1]-p2[1]
-	return math.sqrt(dx**2+dy**2)
-
-
-def generate_local_map_from_point(pt_index, points, num_neighbors):
+def generate_local_map_from_point(pt_index, points, num_neighbors, distance_func):
 	center_pt = points[pt_index]
-	sorted_pts = sorted(range(len(points)), key = lambda i: L2_distance(points[i], center_pt))
+	
+	# Allows for distance normalization
+	global distance_normalizer
+	distance_normalizer.append(numpy.mean(list(map(lambda i: distance_func(points[i], center_pt), range(len(points))))))
+
+	sorted_pts = sorted(range(len(points)), key = lambda i: distance_func(points[i], center_pt))
 	return sorted_pts[:num_neighbors]
