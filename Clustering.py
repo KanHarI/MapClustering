9a18fbce0087a926df4a4a3a3bf9b9e88b7721e8
DESKTOP-U7D79JB\KanHar <knaan.harpaz@gmail.com>
2018-09-01 12:03:35 +0300

    Split code and added functionality

--- Clustering.py
+++ Clustering.py
@@ -21,27 +21,22 @@ def geographic_distance(p1, p2):
 	return geopy.distance.vincenty(p1, p2).miles
 
 
-def take_while(generator, cond):
-	for g in generator:
-		if not cond(g):
-			raise StopIteration
-		yield g
-
-
-class MapClusterer:
-	def __init__(self, points, num_neighbors, distance_func = L2_distance, max_distance=None):
-		print("Initializing MapClusterer object:")
-		self.points = points
-		self.size = len(self.points)
-		self.num_neighbors = num_neighbors
-		self.max_distance = max_distance
+class DefaultLinkFinder:
+	def __init__(self, max_distance=None, num_neighbors=10, distance_func=L2_distance):
 		self.distance_func = distance_func
+		self.max_distance = max_distance
+		self.num_neighbors = num_neighbors
+
+	def run(self, points):
+		self.size = len(points)
+		self.points = points
 		print("Calculating point distances...")
 		self.calculate_point_distances()
 		print("Finding links...")
 		self.find_links()
 		print("Creating graph...")
 		self.create_graph()
+		return self.graph
 
 	def calculate_point_distances(self):
 		point_distances = []
@@ -53,22 +48,25 @@ class MapClusterer:
 					range(self.size)
 					)))
 				)
-
 		self.point_distances = np.array(point_distances)
 
 
 	def find_links(self):
-		self.links = np.array(
-			list(
-				map(
-					lambda center_pt: np.array(list(take_while(
-						sorted(
-							range(self.size),
-							key=lambda i: self.point_distances[center_pt][i]
-							),
-						lambda i: (self.max_distance is None) or (self.point_distances[center_pt][i] < self.max_distance)))[:self.num_neighbors]),
-					range(self.size))))
-
+		self.links = []
+		for center_pt in progressbar.progressbar(range(self.size)):
+			self.links.append(self.find_neighbors(center_pt))
+		self.links=np.array(self.links)
+	
+	def find_neighbors(self, center_pt):
+		possible_neighbors = []
+		for dst in range(self.size):
+			if self.max_distance is not None and self.point_distances[center_pt][dst] > self.max_distance:
+				continue
+			possible_neighbors.append(dst)
+		possible_neighbors.sort(key=lambda i: self.point_distances[center_pt][i])
+		if self.num_neighbors:
+			possible_neighbors = possible_neighbors[:self.num_neighbors]
+		return np.array(possible_neighbors)
 
 	def create_graph(self):
 		self.graph = nx.DiGraph()
@@ -77,6 +75,48 @@ class MapClusterer:
 		for pi in range(self.size):
 			for di in self.links[pi]:
 				self.graph.add_edge(pi,di)
+
+
+class MultiradiusLinkFinder(DefaultLinkFinder):
+	def __init__(self, distance_neighbor_list, distance_func=L2_distance):
+		super().__init__(0,0,distance_func)
+		# A list of distance-number of neighbors pairs, ordered
+		# from smallest radius and most neighbors to greatest radius
+		# and least neighbors
+		self.distance_neighbor_list = distance_neighbor_list
+
+	def find_neighbors(self, center_pt):
+		max_distance = self.distance_neighbor_list[-1][0]
+		possible_neighbors = []
+		for dst in range(self.size):
+			if max_distance is not None and self.point_distances[center_pt][dst] > max_distance:
+				continue
+			possible_neighbors.append(dst)
+		possible_neighbors.sort(key=lambda i: self.point_distances[center_pt][i])
+
+		distance_neighbor_list_ptr = 0
+		i = 0
+		while i < len(possible_neighbors):
+			if i > self.distance_neighbor_list[distance_neighbor_list_ptr][1]:
+				possible_neighbors = possible_neighbors[:i]
+				break
+			if self.point_distances[center_pt][i] > distance_neighbor_list[distance_neighbor_list_ptr][0]:
+				distance_neighbor_list_ptr += 1
+				if distance_neighbor_list_ptr >= len(distance_neighbor_list):
+					possible_neighbors = possible_neighbors[:i]
+					break
+			i += 1
+
+
+		return np.array(possible_neighbors)
+
+
+class MapClusterer:
+	def __init__(self, points, graph_creator):
+		print("Initializing MapClusterer object:")
+		self.points = points
+		self.size = len(self.points)
+		self.graph = graph_creator.run(self.points)
 
 
 	def run_search(self, sensitivity=7, pr_alpha=0.95):
@@ -94,7 +134,7 @@ class MapClusterer:
 			# Priritizing low-connectivity nodes in the network
 			modified_p_rank = {key: (math.exp((max_rank - value)*sensitivity/max_rank)) for (key, value) in p_rank.items()}
 			
-			# My rank takes into account all neighbors of a possible nodes.
+			# my_rank takes into account all neighbors of a possible nodes.
 			# The iteration is over self.graph.nodes() and not over 
 			# graph.nodes() as sometimes a node already removed from the graph
 			# will be the best match.
--- Test.py
+++ Test.py
@@ -3,7 +3,8 @@ import Generators
 import Clustering
 
 def main():
-	clusterer = Clustering.MapClusterer(Generators.WhiteNoiseGenerator(1000).points, 10, Clustering.L2_distance, 0.05)
+	graph_creator = Clustering.MultiradiusLinkFinder([(25, 1000), (50,10)], Clustering.geographic_distance)
+	clusterer = Clustering.MapClusterer(Generators.JsonGenerator("stores.json").points, graph_creator)
 	r = clusterer.run_search(sensitivity=12)
 	print("Found solution with sensitivity: {1} and pr_alpha: {2} result length: {0}".format(len(r), 12, 0.95))
 	open("result.txt", 'w').write(str(r))
